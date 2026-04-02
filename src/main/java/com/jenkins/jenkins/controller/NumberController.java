f4f7f382291184845a5c5008af02549380c4fd4b
tawfiq akdir <tawfiqakdir@gmail.com>
2026-04-02 21:22:49 +0100

    fix

--- src/main/java/com/jenkins/jenkins/controller/NumberController.java
+++ src/main/java/com/jenkins/jenkins/controller/NumberController.java
@@ -1,10 +1,10 @@
 package com.jenkins.jenkins.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class NumberController {
 
   @GetMapping("/number")
